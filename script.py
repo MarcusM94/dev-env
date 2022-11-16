00c708337881bc68b438a099c740aec0d17a5d94
Marcus Mörtstrand <38329815+MarcusM94@users.noreply.github.com>
2022-11-16 19:47:51 +0100

    Update script.py

--- script.py
+++ script.py
@@ -8,8 +8,10 @@ def main():
     for line in output:
         for word in line:
             if word.upper() == 'FAILED':
+                print("hej")
                 sys.exit(1)
     sys.exit(0)
+    print("hejsan")
 
 
 if __name__ == '__main__':
