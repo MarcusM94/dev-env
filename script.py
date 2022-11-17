8643a4f2205c89a1be7a365fa8aa5b2c6b4d2606
Marcus Mörtstrand <38329815+MarcusM94@users.noreply.github.com>
2022-11-17 14:21:45 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,8 @@ import sys
 
 def main():
     output = sys.stdin.readlines()
-    for line in output:
-        print(line)
-    for line in output:
-        for word in line:
-            print(word.upper())
-    print("hejsan")
-    sys.exit(0)
+    for word in output:
+        print(word)
 
 
 if __name__ == '__main__':
